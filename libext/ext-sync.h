36bfbbadc8c10984cb5c19f427fd1f9d8079dd71
ufasoft <dev@ufasoft.com>
2014-12-08 19:49:10 +0500

    -

--- libext/ext-sync.h
+++ libext/ext-sync.h
@@ -1,11 +1,3 @@
-/*######     Copyright (c) 1997-2013 Ufasoft  http://ufasoft.com  mailto:support@ufasoft.com,  Sergey Pavlov  mailto:dev@ufasoft.com #######################################
-#                                                                                                                                                                          #
-# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;  #
-# either version 3, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the      #
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU #
-# General Public License along with this program; If not, see <http://www.gnu.org/licenses/>                                                                               #
-##########################################################################################################################################################################*/
-
 #pragma once
 
 #if UCFG_WIN32
@@ -58,10 +50,10 @@ public:
 	void AttachCreated(HANDLE h);
 	virtual void Unlock() =0;
 
-	virtual bool Lock(DWORD dwTimeout = INFINITE);
+	virtual bool Lock(UInt32 dwTimeout = INFINITE);
 	void Unlock(Int32 lCount, Int32 *lprevCount = 0);
 #ifdef WDM_DRIVER
-	bool LockEx(DWORD dwTimeout, KPROCESSOR_MODE mode, bool bAlertable);
+	bool LockEx(UInt32 dwTimeout, KPROCESSOR_MODE mode, bool bAlertable);
 #endif
 private:
 	CSyncObject(const class_type&);
@@ -112,7 +104,7 @@ public:
 #endif
 	}
 
-	bool Lock(DWORD dwTimeout = INFINITE);
+	bool Lock(UInt32 dwTimeout = INFINITE);
 	bool TryLock();
 	void Unlock();
 	
