bd4cc1fb064c22fff3d96df311eedcdffc74ba83
ufasoft <dev@ufasoft.com>
2015-02-03 22:28:25 +0500

    -

--- libext.h
+++ libext.h
@@ -1,10 +1,3 @@
-/*######     Copyright (c) 1997-2015 Ufasoft  http://ufasoft.com  mailto:support@ufasoft.com,  Sergey Pavlov  mailto:dev@ufasoft.com #########################################################################################################
-#                                                                                                                                                                                                                                            #
-# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;  either version 3, or (at your option) any later version.          #
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.   #
-# You should have received a copy of the GNU General Public License along with this program; If not, see <http://www.gnu.org/licenses/>                                                                                                      #
-############################################################################################################################################################################################################################################*/
-
 #pragma once
 
 #include <el/inc/ext_config.h>
@@ -473,7 +466,7 @@ typedef unsigned long u_long;
 		typedef wchar_t *BSTR;
 		typedef void *HANDLE;
 		typedef int SOCKET;
-		typedef long LONG_PTR;
+//!!!R		typedef long LONG_PTR;
 		typedef unsigned long DWORD_PTR;
 		typedef int64_t INT64;
 		typedef uint64_t UINT64;
