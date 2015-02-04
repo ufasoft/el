ebd9cce041fd988ae0e503b8f9aa19d30df78e92
ufasoft <dev@ufasoft.com>
2015-02-04 14:18:46 +0500

    -

--- libext/ext-stream.h
+++ libext/ext-stream.h
@@ -1,11 +1,3 @@
-/*######     Copyright (c) 1997-2013 Ufasoft  http://ufasoft.com  mailto:support@ufasoft.com,  Sergey Pavlov  mailto:dev@ufasoft.com #######################################
-#                                                                                                                                                                          #
-# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;  #
-# either version 3, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the      #
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU #
-# General Public License along with this program; If not, see <http://www.gnu.org/licenses/>                                                                               #
-##########################################################################################################################################################################*/
-
 #pragma once
 
 namespace Ext {
@@ -30,34 +22,32 @@ public:
 	virtual size_t Read(void *buf, size_t count) const { Throw(E_NOTIMPL); }
 	virtual void ReadBuffer(void *buf, size_t count) const;
 	virtual int ReadByte() const;
-	virtual Int64 Seek(Int64 offset, SeekOrigin origin) const { Throw(E_NOTIMPL); }		// mandatory to implement where put_Position implemented
+	virtual int64_t Seek(int64_t offset, SeekOrigin origin) const { Throw(E_NOTIMPL); }		// mandatory to implement where put_Position implemented
 	
 	virtual void ReadBufferAhead(void *buf, size_t count) const { Throw(E_NOTIMPL); }
 	virtual void Close() const {}
 	virtual void Flush() {}
 
-	virtual UInt64 get_Length() const { 
-		UInt64 curPos = Position;
-		UInt64 endPos = Seek(0, SeekOrigin::End);
+	virtual uint64_t get_Length() const { 
+		uint64_t curPos = Position;
+		uint64_t endPos = Seek(0, SeekOrigin::End);
 		put_Position(curPos);
 		return endPos;
 	}
-	DEFPROP_VIRTUAL_GET_CONST(UInt64, Length);
+	DEFPROP_VIRTUAL_GET_CONST(uint64_t, Length);
 
-	virtual UInt64 get_Position() const {
+	virtual uint64_t get_Position() const {
 		return Seek(0, SeekOrigin::Current);		
 	}
-	virtual void put_Position(UInt64 pos) const {
+	virtual void put_Position(uint64_t pos) const {
 		Seek(pos, SeekOrigin::Begin);
 	}
-	DEFPROP_VIRTUAL_CONST_CONST(UInt64, Position);	
+	DEFPROP_VIRTUAL_CONST_CONST(uint64_t, Position);	
 
-	void CopyTo(Stream& dest, size_t bufsize = 4096) const;
+	void CopyTo(Stream& dest, size_t bufsize = 8192) const;
 
 protected:
 	Stream();
-private:
-
 };
 
 ENUM_CLASS(CompressionMode) {
@@ -86,7 +76,7 @@ protected:
 	virtual void InitImp() const;
 private:
 	void ReadPortion() const;
-	void WritePortion(const void *buf, size_t count, int flush);
+	void WritePortion(const void *ibuf, size_t count, int flush);
 };
 
 class GZipStream : public CompressStream {
