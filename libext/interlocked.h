8a0862c1b9f2d07bd1caee092ab3e5c2232656cd
ufasoft <dev@ufasoft.com>
2015-02-04 11:54:19 +0500

    -

--- libext/interlocked.h
+++ libext/interlocked.h
@@ -1,38 +1,4 @@
-/*######     Copyright (c) 1997-2013 Ufasoft  http://ufasoft.com  mailto:support@ufasoft.com,  Sergey Pavlov  mailto:dev@ufasoft.com #######################################
-#                                                                                                                                                                          #
-# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;  #
-# either version 3, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the      #
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU #
-# General Public License along with this program; If not, see <http://www.gnu.org/licenses/>                                                                               #
-##########################################################################################################################################################################*/
-
 #pragma once
-
-/*!!!
-typedef Ext::byte u8;
-
-inline void barrier() {}
-
-#define __KERNEL__
-#define CONFIG_X86_32
-#define CONFIG_M686
-#define HZ 100
-
-#define new _new
-#define false _false
-#define true _true
-#define _I386_PAGE_H
-#define _I386_BITOPS_H
-#define _LINUX_BITOPS_H
-#define __ASM_I386_PROCESSOR_H
-#include <asm/alternative.h>
-#include <asm/atomic.h>
-#undef new
-#undef false
-#undef true
-#undef __KERNEL__
-*/
-
 
 
 #ifdef _MSC_VER
@@ -88,48 +54,48 @@ long  __cdecl _InterlockedExchangeAdd(long * volatile Addend, long Value);
 
 
 #if defined(_MSC_VER) && !defined(__INTRIN_H_)
-	extern "C" long __cdecl _InterlockedCompareExchange (long volatile *, long, long);
+	extern "C" long __cdecl _InterlockedCompareExchange(long volatile *, long, long);
+	extern "C" char __cdecl _InterlockedCompareExchange8(char volatile *, char, char);
+	extern "C" long __cdecl _InterlockedIncrement(long volatile *Destination);
+	extern "C" long __cdecl _InterlockedDecrement(long volatile *Destination);
+	extern "C" long _InterlockedAnd(long volatile *Destination, long Value);
+	extern "C" long _InterlockedOr(long volatile *Destination, long Value);
+	extern "C" long _InterlockedXor(long volatile *Destination, long Value);
+	extern "C" long __cdecl _InterlockedExchange(long volatile *Destination, long Value);
+	extern "C" char __cdecl _InterlockedExchange8(char volatile *Destination, char Value);
+	extern "C" long __cdecl _InterlockedExchangeAdd(long volatile *, long);
+	extern "C" int64_t __cdecl _InterlockedCompareExchange64(int64_t volatile *, int64_t, int64_t);
+
 #	ifdef _M_ARM
 #	define _InterlockedCompareExchange InterlockedCompareExchange
-#else
+#	define _InterlockedIncrement InterlockedIncrement
+#	define _InterlockedDecrement InterlockedDecrement
+#	else
 #	pragma intrinsic(_InterlockedCompareExchange)
-#endif
+#	pragma intrinsic(_InterlockedCompareExchange8)
+#	pragma intrinsic(_InterlockedIncrement)
+#	pragma intrinsic(_InterlockedDecrement)
+#	endif // !_M_ARM
 
-	extern "C" long __cdecl _InterlockedIncrement (long volatile *Destination);
-#	ifdef _M_ARM
-#		define _InterlockedIncrement InterlockedIncrement
-#	else
-#		pragma intrinsic(_InterlockedIncrement)
-#	endif
+#pragma intrinsic(_InterlockedAnd)
+#pragma intrinsic(_InterlockedOr)
+#pragma intrinsic(_InterlockedXor)
+#pragma intrinsic(_InterlockedExchange)
+#pragma intrinsic(_InterlockedExchange8)
+#pragma intrinsic(_InterlockedExchangeAdd)
+#pragma intrinsic(_InterlockedCompareExchange64)
 
-	extern "C" long __cdecl _InterlockedDecrement (long volatile *Destination);
-#	ifdef _M_ARM
-#		define _InterlockedDecrement InterlockedDecrement
-#	else
-#		pragma intrinsic(_InterlockedDecrement)
-#	endif
+#	if UCFG_PLATFORM_X64
+	extern "C" int64_t __cdecl _InterlockedExchange64(int64_t volatile *, int64_t);
+#	pragma intrinsic(_InterlockedExchange64)
+#	endif // UCFG_PLATFORM_X64
 
-
-	extern "C" long _InterlockedAnd (long volatile *Destination, long Value );
-#	pragma intrinsic(_InterlockedAnd)
-
-	extern "C" long _InterlockedOr (long volatile *Destination, long Value );
-#	pragma intrinsic(_InterlockedOr)
-
-	extern "C" long _InterlockedXor (long volatile *Destination, long Value );
-#	pragma intrinsic(_InterlockedXor)
-
-	extern "C" long __cdecl _InterlockedExchange (long volatile *Destination, long Value );
-#	pragma intrinsic(_InterlockedExchange)
-
-	extern "C" long __cdecl _InterlockedExchangeAdd(long volatile *, long);
-#	pragma intrinsic(_InterlockedExchangeAdd)
 
 
 //	extern "C" long _cdecl InterlockedAdd(long volatile *Destination, long Valu );
 //!!! IPF only #	pragma intrinsic(_InterlockedAdd)
 
-#	if _MSC_VER >= 1700 || defined(_M_X64)
+#	if _MSC_VER >= 1700 /*&& (_MSC_VER < 1900 || UCFG_WDM)*/ || defined(_M_X64)		//!!!?
 	extern "C" void * __cdecl _InterlockedCompareExchangePointer(void * volatile * _Destination, void * _Exchange, void * _Comparand);
 #		pragma intrinsic(_InterlockedCompareExchangePointer)
 #	endif
@@ -142,48 +108,39 @@ long  __cdecl _InterlockedExchangeAdd(long * volatile Addend, long Value);
 
 namespace Ext {
 
-class NonInterlocked {
-public:
-	template <class T> static T Increment(T& v) { return ++v; }
-	template <class T> static T Decrement(T& v) { return --v; }
-};
-
-
-
-
 #ifdef __GNUC__
 
 
 class Interlocked {
 public:
-	static Int32 Add(volatile Int32& v, Int32 add) {
+	static int32_t Add(volatile int32_t& v, int32_t add) {
 		return __sync_add_and_fetch(&v, add);
 
 /*!!!
-		Int32 r = add;
+		int32_t r = add;
 		asm volatile("lock; xaddl %0, %1" : "=r"(r), "+m"(v) : "0"(r) : "memory");
 		return r+add; */
 	}
 
-	static Int32 Increment(volatile Int32& v) {
+	static int32_t Increment(volatile int32_t& v) {
 		return Add(v, 1);
 	}
 
-	static Int32 Decrement(volatile Int32& v) {
+	static int32_t Decrement(volatile int32_t& v) {
 		return Add(v, -1);
 	}
 
 
-	static Int32 Exchange(volatile Int32& v, Int32 n) {
+	static int32_t Exchange(volatile int32_t& v, int32_t n) {
 		return __sync_lock_test_and_set(&v, n);
-/*		Int32 r = n;
+/*		int32_t r = n;
 		asm volatile("xchg %0, %1" : "=r"(r), "+m"(v) : "0"(r) : "memory");
 		return r;*/
 	}
 
-	static Int32 CompareExchange(volatile Int32& d, Int32 e, Int32 c) {
+	static int32_t CompareExchange(volatile int32_t& d, int32_t e, int32_t c) {
 		return __sync_val_compare_and_swap(&d, c, e);
-/*!!!		Int32 prev = c;
+/*!!!		int32_t prev = c;
 		asm volatile( "lock; cmpxchg %3,%1"
 			: "=a" (prev), "=m" (d)
 			: "0" (prev), "r" (e)
@@ -212,9 +169,12 @@ public:
 	template <class T> static T Increment(volatile T& v) noexcept { return T(_InterlockedIncrement((LONG*)(&v))); }
 	template <class T> static T Decrement(volatile T& v) noexcept { return T(_InterlockedDecrement((LONG*)(&v))); }
 #	else
-	template <class T> static T Increment(volatile T& v) noexcept { return T(_InterlockedIncrement((volatile LONG*)(&v))); }
-	template <class T> static T Decrement(volatile T& v) noexcept { return T(_InterlockedDecrement((volatile LONG*)(&v))); }
-#	endif
+//	template <class T> __forceinline static T Increment(volatile T& v) noexcept { return T(_InterlockedIncrement((volatile long*)(&v))); }
+//	template <class T> __forceinline static T Decrement(volatile T& v) noexcept { return T(_InterlockedDecrement((volatile long*)(&v))); }
+
+	__forceinline static int32_t Increment(volatile int32_t& v) noexcept { return _InterlockedIncrement((volatile long*)(&v)); }
+	__forceinline static int32_t Decrement(volatile int32_t& v) noexcept { return _InterlockedDecrement((volatile long*)(&v)); }
+#	endif // !UCFG_WCE
 
 /*!!!R	template <class T> static T Exchange(T &d, T e)
 	{
@@ -231,13 +191,11 @@ public:
 #endif
 	}
 
-	static Int32 Exchange(volatile Int32& d, Int32 e) noexcept {
-		return _InterlockedExchange(reinterpret_cast<long*>(const_cast<Int32*>(&d)), e);
-	}
-	
-	static Int32 CompareExchange(volatile Int32& d, Int32 e, Int32 c) noexcept {
-		return _InterlockedCompareExchange(reinterpret_cast<long*>(const_cast<Int32*>(&d)), e, c);
-	}
+	static int32_t Exchange(volatile int32_t& d, int32_t e) noexcept { return _InterlockedExchange(reinterpret_cast<long*>(const_cast<int32_t*>(&d)), e); }
+	static char Exchange(volatile char& d, char v) noexcept { return _InterlockedExchange8(&d, v); }
+
+	static int32_t CompareExchange(volatile int32_t& d, int32_t e, int32_t c) noexcept { return _InterlockedCompareExchange(reinterpret_cast<long*>(const_cast<int32_t*>(&d)), e, c); }
+	static char CompareExchange(volatile char& d, char e, char c) noexcept { return _InterlockedCompareExchange8(&d, e, c); }
 
 	template <typename T>
 	static T *CompareExchange(T *volatile & d, T *e, T *c) noexcept {
@@ -248,24 +206,30 @@ public:
 #endif
 	}
 
-	static Int32 ExchangeAdd(volatile Int32& d, Int32 v) noexcept {
-		return _InterlockedExchangeAdd(reinterpret_cast<long*>(const_cast<Int32*>(&d)), v);
+#	if defined(_M_X64)
+	static intptr_t CompareExchange(volatile intptr_t& d, intptr_t e, intptr_t c) noexcept {
+		return (intptr_t)CompareExchange(reinterpret_cast<int * volatile&>(d), (int*)e, (int*)c);
+	}
+#	endif
+
+	static int32_t ExchangeAdd(volatile int32_t& d, int32_t v) noexcept {
+		return _InterlockedExchangeAdd(reinterpret_cast<long*>(const_cast<int32_t*>(&d)), v);
 	}
 
 #if LONG_MAX == 2147483647
-	static Int32 And(volatile Int32& d, Int32 v) {
+	static int32_t And(volatile int32_t& d, int32_t v) {
 		return _InterlockedAnd(reinterpret_cast<volatile long*>(&d), v);
 	}
 
-	static Int32 Or(volatile Int32& d, Int32 v) {
+	static int32_t Or(volatile int32_t& d, int32_t v) {
 		return _InterlockedOr(reinterpret_cast<volatile long*>(&d), v);
 	}
 
-	static Int32 Xor(volatile Int32& d, Int32 v) {
+	static int32_t Xor(volatile int32_t& d, int32_t v) {
 		return _InterlockedXor(reinterpret_cast<volatile long*>(&d), v);
 	}
 
-	static Int32 Add(volatile Int32& d, Int32 v) {
+	static int32_t Add(volatile int32_t& d, int32_t v) {
 		return _InterlockedExchangeAdd(reinterpret_cast<volatile long*>(&d), v) + v;
 	}
 #endif
@@ -282,11 +246,11 @@ public:
 	}
 
 	inline int atomic_cmpset_int(volatile int *dst, int old, int n) {
-		return Interlocked::CompareExchange(*(volatile Int32 *)dst,n,old) == old;
+		return Interlocked::CompareExchange(*(volatile int32_t *)dst,n,old) == old;
 	}
 
 	inline int atomic_cmpset_int(volatile unsigned int *dst, int old, int n) {
-		return Interlocked::CompareExchange(*(volatile Int32*)dst,n,old) == old;
+		return Interlocked::CompareExchange(*(volatile int32_t*)dst,n,old) == old;
 	}
 
 #endif
