c6ebc032a9959b3a1ed14bd596fa37b8a9eda423
ufasoft <dev@ufasoft.com>
2015-02-04 15:09:57 +0500

    -

--- libext/ext-posix-win.h
+++ libext/ext-posix-win.h
@@ -1,14 +1,24 @@
-/*######     Copyright (c) 1997-2014 Ufasoft  http://ufasoft.com  mailto:support@ufasoft.com,  Sergey Pavlov  mailto:dev@ufasoft.com #######################################
-#                                                                                                                                                                          #
-# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;  #
-# either version 3, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the      #
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU #
-# General Public License along with this program; If not, see <http://www.gnu.org/licenses/>                                                                               #
-##########################################################################################################################################################################*/
-
 #pragma once
 
 // Windows definitions for non-Windows environment
+
+#ifndef _MINWINDEF_
+	typedef intptr_t INT_PTR;
+	typedef int                 BOOL;
+	typedef unsigned int        UINT;
+	typedef INT_PTR (__stdcall *FARPROC)();
+	typedef uint32_t DWORD;
+	typedef byte BYTE;
+	typedef char CHAR;
+	typedef unsigned short USHORT;
+
+#	ifndef WDM_DRIVER
+		typedef DWORD LCID;
+#	endif
+#	define WINAPI      __stdcall
+#	define INVALID_HANDLE_VALUE ((HANDLE)(LONG_PTR)-1)
+#	define INFINITE            0xFFFFFFFF  // Infinite timeout
+#endif // _MINWINDEF_
 
 
 typedef struct sockaddr_in SOCKADDR_IN;
