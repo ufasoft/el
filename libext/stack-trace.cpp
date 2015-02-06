4e2a1f11ca3120c5323529eef123ab56622b393e
ufasoft <dev@ufasoft.com>
2015-02-06 20:45:16 +0500

    -

--- libext/stack-trace.cpp
+++ libext/stack-trace.cpp
@@ -1,11 +1,3 @@
-/*######     Copyright (c) 1997-2013 Ufasoft  http://ufasoft.com  mailto:support@ufasoft.com,  Sergey Pavlov  mailto:dev@ufasoft.com #######################################
-#                                                                                                                                                                          #
-# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;  #
-# either version 3, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the      #
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU #
-# General Public License along with this program; If not, see <http://www.gnu.org/licenses/>                                                                               #
-##########################################################################################################################################################################*/
-
 #include <el/ext.h>
 
 #if UCFG_WIN32
@@ -121,7 +113,7 @@ String CStackTrace::ToString() const {
 
 	ostringstream os;
 	for (size_t i=0; i<Frames.size(); ++i) {
-		UInt64 addr = Frames[i];
+		uint64_t addr = Frames[i];
 		os << setw(AddrSize*2) << setfill('0') << hex << addr;
 
 #if UCFG_WIN32
