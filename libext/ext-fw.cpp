fde00445ef147502a2b2c2f39ead00b204d13391
ufasoft <dev@ufasoft.com>
2015-02-25 19:50:44 +0500

    -

--- libext/ext-fw.cpp
+++ libext/ext-fw.cpp
@@ -1512,7 +1512,7 @@ bool ProcessObj::Start() {
 		String filename = StartInfo.FileName.native();
 		argv.push_back(filename);
 		vector<String> v = ParseCommandLine(StartInfo.Arguments);
-		TRC(5, "Args: " << v);
+		TRC(5, "Args:\n" << v << "----------------");
 		for (size_t i=0; i<v.size(); ++i)
 			argv.push_back(v[i]);
 		argv.push_back(nullptr);
