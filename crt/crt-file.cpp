da0d39d54cfa59bd729a65284ea833d0c8d814c8
ufasoft <dev@ufasoft.com>
2015-05-03 16:34:05 +0500

    -

---
+++ crt/crt-file.cpp
@@ -0,0 +1,16 @@
+/*######   Copyright (c) 2015 Ufasoft  http://ufasoft.com  mailto:support@ufasoft.com,  Sergey Pavlov  mailto:dev@ufasoft.com ####
+#                                                                                                                                     #
+# 		See LICENSE for licensing information                                                                                         #
+#####################################################################################################################################*/
+
+#include <el/ext.h>
+
+
+extern "C" {
+
+int __cdecl API_close(int fh) {
+	return _close(fh);
+}
+
+
+} // "C"
