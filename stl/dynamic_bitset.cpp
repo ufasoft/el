5d543e2a656dcb35be74c7bdd3b63e4562348644
ufasoft <dev@ufasoft.com>
2018-10-08 03:47:01 +0500

    -

--- stl/dynamic_bitset.cpp
+++ stl/dynamic_bitset.cpp
@@ -1,13 +1,18 @@
+/*######   Copyright (c) 2012-2018 Ufasoft  http://ufasoft.com  mailto:support@ufasoft.com,  Sergey Pavlov  mailto:dev@ufasoft.com ####
+#                                                                                                                                     #
+# 		See LICENSE for licensing information                                                                                         #
+#####################################################################################################################################*/
+
 #include <el/ext.h>
 
-#include "dynamic_bitset"
+#include EXT_HEADER_DYNAMIC_BITSET
 
 namespace ExtSTL {
 using namespace Ext;
 
 void dynamic_bitsetBase::replace(size_type pos, bool val) {
-	byte& bref = ByteRef(pos);
-	bref = bref & ~(1 << (pos & 7)) | (byte(val) << (pos & 7));
+	uint8_t& bref = ByteRef(pos);
+	bref = bref & ~(1 << (pos & 7)) | (uint8_t(val) << (pos & 7));
 }
 
 void dynamic_bitsetBase::reset(size_type pos) {
@@ -20,4 +25,3 @@ void dynamic_bitsetBase::flip(size_type pos) {
 
 
 }  // ExtSTL::
-
