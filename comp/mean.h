b84f54219996c0fc87d90d0f7892ec6a8547dbac
ufasoft <dev@ufasoft.com>
2019-08-28 20:00:21 -0700

    Update

--- comp/mean.h
+++ comp/mean.h
@@ -1,3 +1,8 @@
+/*######   Copyright (c) 2017-2019 Ufasoft  http://ufasoft.com  mailto:support@ufasoft.com,  Sergey Pavlov  mailto:dev@ufasoft.com ####
+#                                                                                                                                     #
+# 		See LICENSE for licensing information                                                                                         #
+#####################################################################################################################################*/
+
 #pragma once
 
 namespace Ext {
@@ -14,25 +19,25 @@ public:
 	size_t MaxSize;
 
 	MeanCalculator(size_t maxSize = 10)
-		:	MaxSize(maxSize)
+		: MaxSize(maxSize)
 	{
 	}
 
 	void AddValue(const T& v) {
 		if (m_deq.size() >= MaxSize)
 			m_deq.pop_front();
-		Item item = { DateTime::UtcNow(), v };
+		Item item = { Clock::now(), v };
 		m_deq.push_back(item);
 	}
 
 	double FindMeanValue() const {
 		if (m_deq.empty())
 			return T();
-		double sec = duration_cast<milliseconds>(DateTime::UtcNow() - m_deq.front().Timestamp).count() / 1000.;		
+		double sec = duration_cast<milliseconds>(Clock::now() - m_deq.front().Timestamp).count() / 1000.;
 		if (sec < 0.1)
 			return 0;
 		T sum = T();
-		for (typename std::deque<Item>::const_iterator it=m_deq.begin(); it!=m_deq.end(); ++it)
+		for (typename std::deque<Item>::const_iterator it = m_deq.begin(); it != m_deq.end(); ++it)
 			sum += it->Value;
 		return sum / sec;
 	}
